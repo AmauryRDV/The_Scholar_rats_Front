38ade7d5e8213d9d45bb00c54c6b29299c75bc78
EnzoDA <enzodasilvaribeiro@gmail.com>
2023-03-29 11:32:04 +0200

    page cartouche

--- src/views/Cartouche.vue
+++ src/views/Cartouche.vue
@@ -5,6 +5,14 @@
     
 
     <section class="main">
+        <h1>CARTOUCHE</h1>
+    <div class="cartouche">
+        <p>academie : Besançon </p> <p>Session : 02/04/2023</p> <p>Modele : EN</p>
+        <p>Examen ou Concours : BTS SIO</p> <p>Serie : 78290</p>
+        <p>Spécialité/Option : SLAM</p> <p>Repère de l'épreuve : 78890</p>
+        <p>Epreuve/Sous-épreuve : Cyber-Sécurité</p>
+        </div>
+
         <div class="temps">
             <label for="debut">Debut de l'épreuve : 8H00</label>
             <label for="fin">Fin de l'épreuve : 12H00</label>
@@ -14,13 +22,9 @@
     <div class="containe_horloge">
     <iframe width='487' height='215' style='border:none!important;background:none!important;background:transparent!important' marginheight='0' marginwidth='0' frameborder='0' scrolling='no' comment='/*defined*/' src='https://dayspedia.com/if/digit/?v=1&iframe=eyJ3LTEyIjp0cnVlLCJ3LTExIjpmYWxzZSwidy0xMyI6dHJ1ZSwidy0xNCI6ZmFsc2UsInctMTUiOnRydWUsInctMTEwIjpmYWxzZSwidy13aWR0aC0wIjp0cnVlLCJ3LXdpZHRoLTEiOmZhbHNlLCJ3LXdpZHRoLTIiOmZhbHNlLCJ3LTE2IjoiMjRweCIsInctMTkiOiI5NiIsInctMTciOiIxNiIsInctMjEiOnRydWUsImJnaW1hZ2UiOi0xLCJiZ2ltYWdlU2V0IjpmYWxzZSwidy0yMWMwIjoiI2ZmZmZmZiIsInctMCI6dHJ1ZSwidy0zIjpmYWxzZSwidy0zYzAiOiIjMzQzNDM0Iiwidy0zYjAiOiIxIiwidy02IjoiIzM0MzQzNCIsInctMjAiOnRydWUsInctNCI6IiNmZmZmZmYiLCJ3LTE4IjpmYWxzZSwidy13aWR0aC0yYy0wIjoiMzAwIiwidy0xMTUiOmZhbHNlfQ==&lang=fr&cityid=1020'></iframe>
     </div>
-    <div class="cartouche">
-        6
-
-        </div>
-        <div class="description">
+    <div class="description">
             <form action="description">
-                <textarea name="description" id="description" placeholder="Description" style="margin-left: 3%;resize: none;"  cols="100" rows="8">
+                <textarea name="description" id="description" placeholder="Description" style="color:black;resize: none;"  cols="140" rows="8">
 
                 </textarea>
             </form>
