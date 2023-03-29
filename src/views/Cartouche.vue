d8b861b07205b279436f3e7ae99e4c3d5860d42f
EnzoDA <enzodasilvaribeiro@gmail.com>
2023-03-29 10:43:55 +0200

    voila

--- src/views/Cartouche.vue
+++ src/views/Cartouche.vue
@@ -2,22 +2,22 @@
     <header>
     <link rel="stylesheet" href="{{ asset\cartouche.css }}">
     </header>
+    
 
     <section class="main">
         <div class="temps">
-            <label for="debut"></label>
-
+            <label for="debut">Debut de l'épreuve : 8H00</label>
+            <label for="fin">Fin de l'épreuve : 12H00</label>
+            <label for="sortie">Mise en loge : 2H00</label>
+            <label for="tier_temp">Tier-temp : 13H30</label>
         </div>
-
-        <div class="horloge">
-5
-        </div>
-
-        <div class="cartouche">
+    <div class="containe_horloge">
+    <iframe width='487' height='215' style='border:none!important;background:none!important;background:transparent!important' marginheight='0' marginwidth='0' frameborder='0' scrolling='no' comment='/*defined*/' src='https://dayspedia.com/if/digit/?v=1&iframe=eyJ3LTEyIjp0cnVlLCJ3LTExIjpmYWxzZSwidy0xMyI6dHJ1ZSwidy0xNCI6ZmFsc2UsInctMTUiOnRydWUsInctMTEwIjpmYWxzZSwidy13aWR0aC0wIjp0cnVlLCJ3LXdpZHRoLTEiOmZhbHNlLCJ3LXdpZHRoLTIiOmZhbHNlLCJ3LTE2IjoiMjRweCIsInctMTkiOiI5NiIsInctMTciOiIxNiIsInctMjEiOnRydWUsImJnaW1hZ2UiOi0xLCJiZ2ltYWdlU2V0IjpmYWxzZSwidy0yMWMwIjoiI2ZmZmZmZiIsInctMCI6dHJ1ZSwidy0zIjpmYWxzZSwidy0zYzAiOiIjMzQzNDM0Iiwidy0zYjAiOiIxIiwidy02IjoiIzM0MzQzNCIsInctMjAiOnRydWUsInctNCI6IiNmZmZmZmYiLCJ3LTE4IjpmYWxzZSwidy13aWR0aC0yYy0wIjoiMzAwIiwidy0xMTUiOmZhbHNlfQ==&lang=fr&cityid=1020'></iframe>
+    </div>
+    <div class="cartouche">
         6
 
         </div>
-        
         <div class="description">
             <form action="description">
                 <textarea name="description" id="description" placeholder="Description" style="margin-left: 3%;resize: none;"  cols="100" rows="8">
@@ -26,7 +26,10 @@
             </form>
         </div>
     </section>
+        
+
 </template>
-<script>
+<script type="text/javascript">
 import styles from "../assets/cartouche.css";
+
 </script>
