3329fa46f371e01a6f1e481eb5df8374eb730a03
AmauryRDV <amaury.richarddevesvrotte@gmail.com>
2023-04-04 10:30:28 +0200

    cadeau enzo

--- src/views/Cartouche.vue
+++ src/views/Cartouche.vue
@@ -1,7 +1,7 @@
 
 <script type="text/javascript">
 import styles from "../assets/cartouche.css";
-import axios from "axios";
+import axios, { formToJSON } from "axios";
 import moment from 'moment';
 
 export default({
@@ -15,6 +15,12 @@ export default({
         loading: false,
         timer:"",
         mescouilles:"",
+        horloge:"",
+        timer2:"",
+        heuredebut:"",
+        heurefin:"",
+        miseenloge:"",
+
 
 
             
@@ -23,33 +29,40 @@ export default({
   beforeMount(){
     this.apiUrl = import.meta.env.VITE_API_URL
     this.getCartouche()
-    this.getAlert()
   },
   mounted() {
     this.timer = setInterval(()=>{
         this.getAlert()
+        if(this.alertes.id>0)
+        {
         confirm("Alerte :"+this.alertes.titre+"\n"+"Infos :"+this.alertes.description+"\n"+"Lien :"+this.alertes.pdf)
-        },6000000)
-        
-  },
+        }
+        this.alertes={};
+
+    },60000)
+    this.timer2 = setInterval(()=>
+    {
+        this.showDate()
+    },1000)
+    },
   methods:{
+        showDate()
+        {
+            this.horloge=moment().format("HH:mm:ss");
+        },
         getCartouche()
         {
         this.loading=true;
         axios.get(this.apiUrl + "api/examen/"+this.$route.params.id)
             .then(
                 (response) => {
-                    console.log(response);
                     this.loading=false;
                     this.cartouche = response.data;
-                    this.macouille=moment("2025-01-01 "+ this.cartouche.epreuve.debutepreuve,"YYYY-DD-MM HH:mm:ss");
-                    this.macouillebleu=moment("2025-01-01 "+ this.cartouche.epreuve.finepreuve,"YYYY-DD-MM HH:mm:ss");
-                    this.pausecouille=moment("2025-01-01 "+ this.cartouche.epreuve.miseenloge,"YYYY-DD-MM HH:mm:ss");
-                    this.stockcouillasse=moment(this.macouillebleu);
-                    console.log(this.macouillebleu);
-                    this.stockcouillasse3=moment(this.macouillebleu);
-                    this.stockcouillasse2=moment(this.stockcouillasse.subtract(this.macouille.hours(),"hours"));
-                    this.pluscouille=moment(this.stockcouillasse3.add(this.stockcouillasse2.hours(),"hours"));
+                    this.heuredebut=moment("2025-01-01 "+ this.cartouche.epreuve.debutepreuve,"YYYY-DD-MM HH:mm:ss");
+                    this.heurefin=moment("2025-01-01 "+ this.cartouche.epreuve.finepreuve,"YYYY-DD-MM HH:mm:ss");
+                    this.miseenloge=moment("2025-01-01 "+ this.cartouche.epreuve.miseenloge,"YYYY-DD-MM HH:mm:ss");
+                    //this.heurefin2=moment(this.heurefin);
+                   // this.test=moment(this.heurefin2.subtract(this.heuredebut.hours(),"hours"));
                 }
             )
         },
@@ -84,13 +97,13 @@ export default({
 
     
         <div class="temps">
-            <label for="debut">Debut de l'épreuve : {{ this.macouille.format("HH:mm") }}</label>
-            <label for="fin">Fin de l'épreuve : {{ this.macouillebleu.format("HH:mm") }}</label>
-            <label for="sortie">Mise en loge : {{ this.pausecouille.format("HH:mm") }}</label>
-            <label for="tier_temp">Tier-temp : {{ this.pluscouille.format("HH:mm") }}</label>
+            <label for="debut">Debut de l'épreuve : {{ this.heuredebut.format("HH:mm") }}</label>
+            <label for="fin">Fin de l'épreuve : {{ this.heurefin.format("HH:mm") }}</label>
+            <label for="sortie">Mise en loge : {{ this.miseenloge.format("HH:mm") }}</label>
+            <label for="tier_temp">Tier-temp : </label>
         </div>
-    <div class="containe_horloge">
-    <iframe width='480' height='215' style="border:none !important;background:none !important;background:transparent !important" marginheight='0' marginwidth='0' frameborder='0' scrolling='no' comment='/*defined*/' src='https://dayspedia.com/if/digit/?v=1&iframe=eyJ3LTEyIjp0cnVlLCJ3LTExIjpmYWxzZSwidy0xMyI6dHJ1ZSwidy0xNCI6ZmFsc2UsInctMTUiOnRydWUsInctMTEwIjpmYWxzZSwidy13aWR0aC0wIjp0cnVlLCJ3LXdpZHRoLTEiOmZhbHNlLCJ3LXdpZHRoLTIiOmZhbHNlLCJ3LTE2IjoiMjRweCIsInctMTkiOiI5NiIsInctMTciOiIxNiIsInctMjEiOnRydWUsImJnaW1hZ2UiOi0xLCJiZ2ltYWdlU2V0IjpmYWxzZSwidy0yMWMwIjoiI2ZmZmZmZiIsInctMCI6dHJ1ZSwidy0zIjpmYWxzZSwidy0zYzAiOiIjMzQzNDM0Iiwidy0zYjAiOiIxIiwidy02IjoiIzM0MzQzNCIsInctMjAiOnRydWUsInctNCI6IiNmZmZmZmYiLCJ3LTE4IjpmYWxzZSwidy13aWR0aC0yYy0wIjoiMzAwIiwidy0xMTUiOmZhbHNlfQ==&lang=fr&cityid=1020'></iframe>
+    <div>
+        <span id='horloge' style="background-color:#219bc3;color:white;font-size:7em;">{{ this.horloge }}</span>
     </div>
     </div>
     </section>
