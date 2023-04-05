4ea7a210f6082d0703118062e5d7cc7e08f64e71
AmauryRDV <amaury.richarddevesvrotte@gmail.com>
2023-04-05 10:37:46 +0200

    Cartouche

--- src/views/Cartouche.vue
+++ src/views/Cartouche.vue
@@ -19,6 +19,9 @@ export default({
         heuredebut:"",
         heurefin:"",
         miseenloge:"",
+        listeAlertes:[],
+        chronometre:moment("2025-01-01 00:00:00","HH:mm:ss"),
+        go:"1",
 
 
 
@@ -26,28 +29,99 @@ export default({
       }
   },
   beforeMount(){
+    // Avant le chargement de la page
     this.apiUrl = import.meta.env.VITE_API_URL
     this.getCartouche()
   },
   mounted() {
+    // Lors de l'ouverture de la page
+
+    //FOnction qui permet de vérifier toutes les 60 secondes si une erreur est présente
     this.timer = setInterval(()=>{
         this.getAlert()
+        //vérifie si il y a un objet donc une erreur présente
         if(Object.keys(this.alertes).length != 0)
         {
         confirm("Alerte :"+this.alertes.titre+"\n"+"Infos :"+this.alertes.description+"\n"+"Lien :"+this.alertes.pdf)
+        //Envoie l'alerte vers le tableau et vide la liste des alertes
+        this.listeAlertes.push({titre: this.alertes.titre, description: this.alertes.description, lien: this.alertes.lien})
         this.alertes={};
         }
-    },10000)
+    },60000)
+
+    //Interval pour l'horloge et le chronomètre
     this.timer2 = setInterval(()=>
     {
+        // Si le chrono est lancée
+        if(this.go==3)
+        {
+            var elem=document.getElementById('chronometre');
+            this.chronometre.add(1,"seconds");
+            if(this.chronometre.hours()>=this.moitieduree.hours())
+            {
+                elem.style.color="orange";
+            }
+            if(this.chronometre.hours()>=this.tqepreuve.hours() && this.tqepreuve.hours()>this.moitieduree.hours())
+            {
+                elem.style.color="darkred";
+            }
+        }
+        //Cette fonction met à jour la date
         this.showDate()
     },1000)
     },
   methods:{
-        showDate()
+    //Définie si on lance le chrono ou qu'on le stop
+        setGo()
         {
+            if (this.go==1) //On lance le chrno
+            {
+            this.go=2;
+            var elem = document.getElementById('chronometre');
+            elem.style.visibility = 'visible';
+            elem.style.display='block';
+            var elem = document.getElementById('lancer');
+            elem.textContent="Stop";
+            var elem = document.getElementById('pause');
+            elem.style.visibility='visible';
+
+            }
+            else //On stop le chrono
+            {
+                this.go=1;
+                var elem = document.getElementById('chronometre');
+                elem.style.visibility = 'hidden';
+                elem.style.display='none';
+                var elem = document.getElementById('lancer');
+                elem.textContent="Lancer";
+            }
+        },
+
+        setPause() //Permet de mettre en pause le chrono ou de relancer le chrono
+        {
+            var elem = document.getElementById('pause');
+            if (elem.textContent=="Pause")
+            {
+                this.go=4; 
+                elem.textContent="Relancer";  
+            }
+            else
+            {
+                this.go=3;
+                elem.textContent="Pause";
+            }
+        },
+        showDate() //initialise l'horloge et le chrono
+        { 
+            if(this.go==2)
+            {
+                this.chronometre=moment("2025-01-01 00:00:00","YYYY-DD-MM HH:mm:ss");
+                this.go=3;
+            }
             this.horloge=moment().format("HH:mm:ss");
         },
+
+        //récupère l'examen depuis l'id et permet de rentrer les infos cartouches
         getCartouche()
         {
         this.loading=true;
@@ -56,15 +130,33 @@ export default({
                 (response) => {
                     this.loading=false;
                     this.cartouche = response.data;
+                    
+                    //Définie les heures de base
                     this.heuredebut=moment("2025-01-01 "+ this.cartouche.epreuve.debutepreuve,"YYYY-DD-MM HH:mm:ss");
                     this.heurefin=moment("2025-01-01 "+ this.cartouche.epreuve.finepreuve,"YYYY-DD-MM HH:mm:ss");
                     this.miseenloge=moment("2025-01-01 "+ this.cartouche.epreuve.miseenloge,"YYYY-DD-MM HH:mm:ss");
+
+                    // Pour calculer la duree de l'épreuve
+                    this.stockheurefin=moment(this.heurefin);
+                    this.duree=moment(this.stockheurefin.subtract(this.heuredebut.hours(),'hours'));
+                    // Pour calculer la moitier de l'épreuve
+                    this.moitieduree=moment(this.duree.hours()/2,'hours');
+                    this.moitiedureestock=moment(this.moitieduree);
+                    // Pour calculer les 3/4 de l'épreuve
+                    this.temp=moment(this.moitiedureestock.hours()/2,'hours');
+                    this.tqepreuve=(this.moitiedureestock.add(this.temp.hours(),'hours'));
+
+                    //Calcul du tier-temps
+                    this.stockduree=moment(this.duree);
+                    this.stock=moment(this.stockduree.hours()/3,"hours");
+                    this.stockfin=moment(this.heurefin);
+                    this.tiertemps=moment(this.stockfin.add(this.stock.hours(),"hours"));
 
                 }
             )
         },
 
-        getAlert()
+        getAlert() // Fonction qui recherche les erreurs depuis l'API
         {
             axios.get(this.apiUrl + "api/alerte/"+this.$route.params.id)
             .then(
@@ -119,10 +211,10 @@ export default({
                     <th>Description</th>
                     <th>Lien</th>
                 </tr>
-                <tr>
-                    <td>afzfafza</td>
-                    <td>zaffza</td>
-                    <td>azfzaffzaf</td>
+                <tr v-for="alerte in this.listeAlertes">
+                    <td>{{ alerte.titre }}</td>
+                    <td>{{ alerte.description }}</td>
+                    <td>{{ alerte.lien }}</td>
                 </tr>
             </table>
         </div>
@@ -131,10 +223,17 @@ export default({
             <label for="debut">Debut de l'épreuve : {{ this.heuredebut.format("HH:mm") }}</label>
             <label for="fin">Fin de l'épreuve : {{ this.heurefin.format("HH:mm") }}</label>
             <label for="sortie">Mise en loge : {{ this.miseenloge.format("HH:mm") }}</label>
-            <label for="tier_temp">Tier-temp : </label>
+            <label for="tier_temp">Tier-temp : {{ this.tiertemps.format("HH:mm") }}</label>
         </div>
     <div>
-        <span id='horloge' style="color:lightblue;font-size:7em;">{{ this.horloge }}</span>
+        <span id='horloge' style="color:blue;font-size:7em;">{{ this.horloge }}</span>
+    </div>
+    <div style="display: flex;flex-direction: column;align-items: center;">
+        <span id="chronometre" style="color:green;font-size:7em;visibility: hidden;display: none;">{{ this.chronometre.format("HH:mm:ss")}}</span>
+    </div>
+    <div style="display: flex;flex-direction: row;justify-content:center;">
+            <button id="lancer" @click="setGo()">Lancer</button>
+            <button style="visibility:hidden;" id="pause" @click="setPause()">Pause</button>
     </div>
     </div>
     </section>
